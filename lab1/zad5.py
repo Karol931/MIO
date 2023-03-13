b6d817b9262d19edcaa5522f480031f0bed0c26d
Karol Hoerner <kahder6@gmail.com>
2023-03-13 14:12:55 +0100

    done task 5

---
+++ lab1/zad5.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import numpy as np
+from matplotlib import pyplot as plt
+
+data = load_iris().data
+label = load_iris().target
+iterations = [1,2,5,10,20,40,80,200,500,1000]
+accuracy = [0,0,0,0,0,0,0,0,0,0]
+counter = 0
+ratios = [0.1, 0.3, 0.5]
+for c in range(10):
+    train, test, label_train, label_test = train_test_split(data,label,test_size=0.2,train_size=0.8)
+    for i in iterations:
+        neuron = Perceptron(early_stopping = False, max_iter=i)
+        neuron.fit(train,label_train)
+        accuracy[counter] += neuron.score(test, label_test)
+        counter = counter+1
+    counter = 0
+
+for i in range(len(accuracy)):
+    accuracy[i] /= 10
+    print(accuracy[i])
